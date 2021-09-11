9c586e1a606a94dc21acf82ea3ca60a6fb1b1e4f
Ibrokhim98 <33412879+Ibrokhim98@users.noreply.github.com>
2021-09-11 22:25:14 +0500

    Request data from URL on 'load'

--- Book SearcherTests/RemoteBookLoaderTests.swift
+++ Book SearcherTests/RemoteBookLoaderTests.swift
@@ -8,19 +8,46 @@
 import XCTest
 
 class RemoteBookLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
+    
 }
 
 class RemoteBookLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteBookLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteBookLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteBookLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
