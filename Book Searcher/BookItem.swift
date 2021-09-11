4f5d194959a9781c0e121ea57c18e22811ec498c
Ibrokhim98 <33412879+Ibrokhim98@users.noreply.github.com>
2021-09-11 21:45:01 +0500

    Create 'BookItem' struct

---
+++ Book Searcher/BookItem.swift
@@ -0,0 +1,14 @@
+//
+//  BookItem.swift
+//  Book Searcher
+//
+//  Created by Ibrokhim Shukrullaev on 9/11/21.
+//
+
+import Foundation
+
+struct BookItem {
+    let title: String
+    let authors: [String]
+    let thumbnail: String
+}
