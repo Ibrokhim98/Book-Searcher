3b7a02747792a82f4d3e75922be876b358468293
Ibrokhim98 <33412879+Ibrokhim98@users.noreply.github.com>
2021-09-12 21:31:05 +0500

    Create default view with empty  tableView and searchController

--- Book Searcher/AppDelegate.swift
+++ Book Searcher/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = SearchViewController()
+        window?.rootViewController = UINavigationController(rootViewController: SearchViewController())
         window?.makeKeyAndVisible()
         return true
     }
--- Book Searcher/Presentation/SearchScreen/SearchViewController.swift
+++ Book Searcher/Presentation/SearchScreen/SearchViewController.swift
@@ -8,9 +8,66 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    private lazy var searchView: UISearchController = {
+       let searchView = UISearchController(searchResultsController: nil)
+        searchView.obscuresBackgroundDuringPresentation = false
+        searchView.searchBar.placeholder = "Search by Title, Author or Series"
+        searchView.hidesNavigationBarDuringPresentation = false
+        searchView.searchResultsUpdater = self
+        return searchView
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    //MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViews()
+        setViewAppearance()
     }
+    
+    
+    //MARK: - Helper Methods
+    
+    private func setUpViews() {
+        view.addSubview(tableView)
+        navigationItem.searchController = searchView
+        tableView.frame = view.bounds
+    }
+    
+    private func setViewAppearance() {
+        title = "Books"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+
+//MARK: - SearchController Delegate Methods
+extension SearchViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
+
+//MARK: - TableView Delegate Methods
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
 
