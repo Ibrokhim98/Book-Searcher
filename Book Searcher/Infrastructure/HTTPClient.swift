d803cddf225a473d73905cbf38f43c3f428bb739
Ibrokhim98 <33412879+Ibrokhim98@users.noreply.github.com>
2021-09-12 19:45:29 +0500

    Create HTTPClientRepository class

--- Book Searcher/Infrastructure/HTTPClient.swift
+++ Book Searcher/Infrastructure/HTTPClient.swift
@@ -12,3 +12,25 @@ public typealias HTTPClientResponse = (Data, HTTPURLResponse)
 protocol HTTPClient {
     func get(from url: URL, completion: @escaping (Result<HTTPClientResponse, Error>) -> Void)
 }
+
+final class HTTPClientRepository: HTTPClient {
+    
+    func get(from url: URL, completion: @escaping (Result<HTTPClientResponse, Error>) -> Void) {
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                completion(.failure(NetworkError.invalidData))
+                return
+            }
+            
+            completion(.success((data, response)))
+        }
+        task.resume()
+    }
+}
